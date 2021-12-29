af53f1e2beb84cf758b507210ed7fce666e69db0
Hakugin370 <86385071+Hakugin370@users.noreply.github.com>
2021-12-30 07:36:50 +1300

    added my code from 3 new challenges

---
+++ js_snippets/largest_of_four.js
@@ -0,0 +1,10 @@
+function largestOfFour(arr) {
+    let largest = [];
+    for (let elem of arr) {
+      elem.sort(function(a, b){return b-a});
+      largest.push(elem[0])
+    }
+    return largest;
+  }
+  
+  largestOfFour([[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001, 857, 1]]);
