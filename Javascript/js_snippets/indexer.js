cb13d0cb2309424f773be99b266342e9771e6825
Hakugin370 <86385071+Hakugin370@users.noreply.github.com>
2022-01-09 08:44:29 +1300

    Finished indexer challenge

--- Javascript/js_snippets/indexer.js
+++ Javascript/js_snippets/indexer.js
@@ -1,28 +1,22 @@
 function getIndexToIns(arr, num) {
     let copyArr = [...arr];
+    const len = copyArr.length
     copyArr.sort(function(a, b){return a-b});
-    console.log(copyArr);
-    for (let i = 0; i < copyArr.length; i++) {
-        if (num > copyArr[i]) {
-            return i;
+
+    if (len === 0) {
+        return 0;
+    }
+    else if (num > copyArr[len-1]) {
+        console.log(len);
+        return len
+    }    
+    else {
+        for (let i = 0; i < len; i++) {
+            if (num <= copyArr[i]) {
+                return i;
+            }
         }
     }
 }
 
-getIndexToIns([40, 60], 50);
-
-
-function getIndexToIns(arr, num) {
-    let copyArr = [...arr];
-    copyArr.sort(function(a, b){return a-b});
-    console.log(copyArr);
-
-    for (let elem of copyArr) {
-        if (num > elem) {
-            console.log(copyArr.indexOf(elem));
-            return copyArr.indexOf(elem);
-        }
-    }
-}
-
-getIndexToIns([40, 60], 50);
+getIndexToIns([2, 5, 10], 15)
