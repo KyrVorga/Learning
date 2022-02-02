49ec53da764a14f0846aa5163c6fea37498645d5
Hakugin370 <86385071+Hakugin370@users.noreply.github.com>
2022-02-02 19:03:37 +1300

    found better approach online, wrote code myself

--- Javascript/js_snippets/palindrome.js
+++ Javascript/js_snippets/palindrome.js
@@ -4,15 +4,18 @@ function palindrome(str) {
     return reversed === cleaned;
 }
 
-palindrome("race car");
-palindrome("A man, a plan, a canal. Panama");
-palindrome("_eye");
-palindrome("0_0 (: /-\ :) 0-0");
+function palindrome(str) {
+    const pattern = /[_/ ,.:()-]/g
+    let cleaned = str.replace(pattern, '').toLowerCase();
+    let reversed = cleaned.split('').reverse().join('');
+    return reversed === cleaned;
+}
 
 
 function palindrome(str) {
-    let cleaned = str.replace(/[_/ ,.:()-]/g , '').toLowerCase();
-    let reversed = cleaned.split('').reverse().join('');
+    const pattern = /[a-z0-9]/g;
+    let cleaned = str.toLowerCase().match(pattern).join('');
+    let reversed = str.toLowerCase().match(pattern).reverse().join('');
     return reversed === cleaned;
 }
 
