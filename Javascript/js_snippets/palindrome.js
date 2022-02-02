0ce417ad4f81be4b71bed095e16a34a6a60e9406
Hakugin370 <86385071+Hakugin370@users.noreply.github.com>
2022-02-02 16:31:56 +1300

    edited palindrome file, removed garbage code

--- Javascript/js_snippets/palindrome.js
+++ Javascript/js_snippets/palindrome.js
@@ -1,62 +1,22 @@
-function checker(word) {
-	let reversed = word.split('').reverse().join('');
-  if (word === reversed) {
-  	console.log('true');
-  } else {
-  console.log('false');
-  }
-}
-
-checker('level');
-
-function palindrome(word) {
-	let reversed = word.split('').reverse().join('');
-  if (word === reversed) {
-  	return true;
-  } else {
-  return false;
-  }
-}
-
-palindrome('level');
-
 function palindrome(str) {
-  // let reversed = str.split('').reverse().join('');
-  // if (str == reversed) {
-
-  // }
-  let cleaned = str.trim().toLowerCase().split(/\W/g).join('').replace(/_/g, '')
-  let reversed = cleaned.split('').reverse().join(''); 
-  console.log(cleaned, reversed)
-  return reversed === cleaned;
+    let cleaned = str.trim().toLowerCase().split(/\W/g).join('').replace(/_/g, '')
+    let reversed = cleaned.split('').reverse().join(''); 
+    return reversed === cleaned;
 }
 
 palindrome("race car");
-
+palindrome("A man, a plan, a canal. Panama");
+palindrome("_eye");
+palindrome("0_0 (: /-\ :) 0-0");
 
 
 function palindrome(str) {
-  // let reversed = str.split('').reverse().join('');
-  // if (str == reversed) {
-
-  // }
-  str.repla
-  let cleaned = str.trim().toLowerCase().split(/\W/g).join('').replace(/_/g, '')
-  let reversed = cleaned.split('').reverse().join(''); 
-  console.log(cleaned, reversed)
-  return reversed === cleaned;
+    let cleaned = str.replace(/[_/ ,.:()-]/g , '').toLowerCase();
+    let reversed = cleaned.split('').reverse().join('');
+    return reversed === cleaned;
 }
 
 palindrome("race car");
-
-
-
-function palindrome(str) {
-  let cleaned = str.replace(/[_/ ,.:()-]/g , '').toLowerCase();
-  let reversed = cleaned.split('').reverse().join('');
-  return reversed === cleaned;
-}
-
 palindrome("A man, a plan, a canal. Panama");
 palindrome("_eye");
 palindrome("0_0 (: /-\ :) 0-0");
