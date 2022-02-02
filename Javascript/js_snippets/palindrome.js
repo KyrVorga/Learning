a25e9a9b7dac5d03843a603f3f09336eb670c7ca
Hakugin370 <86385071+Hakugin370@users.noreply.github.com>
2022-02-02 16:30:17 +1300

    Finished palindrome

--- Javascript/js_snippets/palindrome.js
+++ Javascript/js_snippets/palindrome.js
@@ -19,3 +19,44 @@ function palindrome(word) {
 }
 
 palindrome('level');
+
+function palindrome(str) {
+  // let reversed = str.split('').reverse().join('');
+  // if (str == reversed) {
+
+  // }
+  let cleaned = str.trim().toLowerCase().split(/\W/g).join('').replace(/_/g, '')
+  let reversed = cleaned.split('').reverse().join(''); 
+  console.log(cleaned, reversed)
+  return reversed === cleaned;
+}
+
+palindrome("race car");
+
+
+
+function palindrome(str) {
+  // let reversed = str.split('').reverse().join('');
+  // if (str == reversed) {
+
+  // }
+  str.repla
+  let cleaned = str.trim().toLowerCase().split(/\W/g).join('').replace(/_/g, '')
+  let reversed = cleaned.split('').reverse().join(''); 
+  console.log(cleaned, reversed)
+  return reversed === cleaned;
+}
+
+palindrome("race car");
+
+
+
+function palindrome(str) {
+  let cleaned = str.replace(/[_/ ,.:()-]/g , '').toLowerCase();
+  let reversed = cleaned.split('').reverse().join('');
+  return reversed === cleaned;
+}
+
+palindrome("A man, a plan, a canal. Panama");
+palindrome("_eye");
+palindrome("0_0 (: /-\ :) 0-0");
