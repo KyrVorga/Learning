a5769c3608896fd191ab998442803404da00e323
Hakugin370 <86385071+Hakugin370@users.noreply.github.com>
2022-02-07 16:23:55 +1300

    made it work at a basic level, making it dynamic.

--- Javascript/js_snippets/weighted_random.js
+++ Javascript/js_snippets/weighted_random.js
@@ -1,3 +1,14 @@
-const db = [['option a',0.5],['option b', 0.25],['option c',0.15],['option d',0.1]]
-random = math.random()
-if (random)
+const db = [['option a',0.1],['option b', 0.15],['option c',0.25],['option d',0.5]]
+
+    function pick(options) {
+    random = Math.random()
+    console.log(random);
+    
+    if (random <= options[0][1]){
+        console.log(options[0])}
+    else {
+        console.log('false')
+    }
+}
+
+pick(db);
