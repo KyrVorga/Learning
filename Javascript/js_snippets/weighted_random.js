9c7fae6e866969409b1cd9c10c9ebcb0c6d8f4b1
Hakugin370 <86385071+Hakugin370@users.noreply.github.com>
2022-02-07 21:22:00 +1300

    created sort method, and started on probability

--- Javascript/js_snippets/weighted_random.js
+++ Javascript/js_snippets/weighted_random.js
@@ -1,21 +1,29 @@
-const db = [['option a',0.3],['option b', 0.15],['option c',0.2],['option d',0.17]]
-
+const db = [["Studying",0,0],["Stretching"],["Cosplay"],["Dancing"],["3d moddeling"],["Iso rings"],["juggling"],["drawing"],["music"],["cleaning"]];
+// name , times picked, probability
 
 function sorter(arr) {
     const copyArr = [...arr];
-    return copyArr.sort((a, b) => b[1] - a[1]);
+    return copyArr.sort((a, b) => b[1] - a[1]).reverse();
 }
+
 function pick(options) {
     
-    random = Math.random();
+    let random = Math.random();
+    console.log(random, options);
+
     for(let i = 0; i < options.length + 1; i++) {
 
         if (random <= options[0][1]){
-            console.log(options[0])}
-        else {
-            console.log('false')
+            console.log(options[0]);
         }
     }
 }
-console.log(sorter(db));
-pick(db);
+function probabilities(object) {
+    const len = object.length + 1;
+    for(let i = 0; i < len; i++) {
+        // calculate probability base on times picked and assign to db 
+    }
+}
+
+
+pick(sorter(db));
