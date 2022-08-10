f7a66ff24bd38eaa350a6d96452630e617d5648d
Rhylei Tremlett <Rhylei@aureate.dev>
2022-08-11 09:51:58 +1200

    web503 class halfway thru

--- Javascript/tester_website/scripts.js
+++
@@ -1,3 +0,0 @@
-function alerting() {
-    return alert("Hello there.");
-}
---
+++ WEB503_week_4_V2/my-app/src/App.js
@@ -0,0 +1,20 @@
+import "./App.css";
+
+function App() {
+	return (
+		<div className="App">
+			<nav>
+				<div>
+					<a class="logo-brand right">To-do List</a>
+					{/* <ul>
+					<li><a href="index.html">Home</a></li>
+					<li><a href="todo.html">To-do list</a></li>
+					<li><a href="contact.html">Contact</a></li>
+				</ul> */}
+				</div>
+			</nav>
+		</div>
+	);
+}
+
+export default App;
